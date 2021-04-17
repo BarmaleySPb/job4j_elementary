8ab9f659320a95f398846932271ed84b78bf6dc8
BarmaleySPb <e.g.zakharov@gmail.com>
2021-04-17 18:08:38 +0300

    4.5. Отладка программы в IDEA [#519539]

--- src/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/ru/job4j/condition/MultipleSwitchWeek.java
@@ -1,20 +1,20 @@
-package ru.job4j.condition;
-
-public class MultipleSwitchWeek {
-    public static int numberOfDay(String name) {
-        return switch (name) {
-            case "Понедельник", "Monday" -> 1;
-            case "Вторник", "Tusday" -> 2;
-            case "Среда", "Thirsday" -> 3;
-            case "Четверг", "Thuersday" -> 4;
-            case "Пятница", "Friday" -> 5;
-            case "Суббота", "Sataday" -> 6;
-            case "Воскресенье", "Sunday" -> 7;
-            default -> -1;
-        };
-    }
-
-    public static void main(String[] args) {
-        System.out.println(numberOfDay("Понедельник"));
-    }
-}
+//package ru.job4j.condition;
+//
+//public class MultipleSwitchWeek {
+//    public static int numberOfDay(String name) {
+//        return switch (name) {
+//            case "Понедельник", "Monday" -> 1;
+//            case "Вторник", "Tusday" -> 2;
+//            case "Среда", "Thirsday" -> 3;
+//            case "Четверг", "Thuersday" -> 4;
+//            case "Пятница", "Friday" -> 5;
+//            case "Суббота", "Sataday" -> 6;
+//            case "Воскресенье", "Sunday" -> 7;
+//            default -> -1;
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(numberOfDay("Понедельник"));
+//    }
+//}
