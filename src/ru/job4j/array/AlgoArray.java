beea3dba5b48a625cbf4f2c58c3336d2658bd283
BarmaleySPb <e.g.zakharov@gmail.com>
2021-04-19 12:23:49 +0300

    5.7. Ипотека [#519653]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,20 @@
+//package ru.job4j.array;
+//
+//public class AlgoArray {
+//    public static void main(String[] args) {
+//        int[] array = new int[] {5, 3, 2, 1, 4};
+//        int temp = array[0];
+//        array[0] = array[3];
+//        array[3] = temp;
+//        for (int i = 0; i < array.length; i++) {
+//            int min = array[i];
+//            if (array[i] < min) {
+//
+//            }
+//        } /* записываем в ячейку с индексом 3 значение временной переменной. */
+//
+//        for (int index = 0; index < array.length; index++) {
+//            System.out.println(array[index]);
+//        }
+//    }
+//}
--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,15 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double debt = amount * (1 + (percent / 100));
-        while (debt > 0) {
+        double debt = 0;
+
+        do {
+            if (debt == 0) {
+                debt = amount * (1 + (percent / 100));
+            }
             debt = (debt - salary) * (1 + (percent / 100));
             year++;
-        }
+        } while (debt > 0);
         return year;
     }
 }
