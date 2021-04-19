72ab57f309c767f9ae783fc9a23806c0bf173b9d
BarmaleySPb <e.g.zakharov@gmail.com>
2021-04-19 12:58:12 +0300

    6.2. Упорядочить массив. [#519590]

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -1,20 +1,26 @@
-//package ru.job4j.array;
-//
-//public class AlgoArray {
-//    public static void main(String[] args) {
-//        int[] array = new int[] {5, 3, 2, 1, 4};
-//        int temp = array[0];
-//        array[0] = array[3];
-//        array[3] = temp;
-//        for (int i = 0; i < array.length; i++) {
-//            int min = array[i];
-//            if (array[i] < min) {
-//
-//            }
-//        } /* записываем в ячейку с индексом 3 значение временной переменной. */
-//
-//        for (int index = 0; index < array.length; index++) {
-//            System.out.println(array[index]);
-//        }
-//    }
-//}
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int ind = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[ind]) {
+                    ind = j;
+                }
+            }
+            int min = array[ind];
+            array[ind] = array[i];
+            array[i] = min;
+        }
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
