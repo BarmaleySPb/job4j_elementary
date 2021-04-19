4ba43094ed58c8a20af0f6d89b5189418e31d7c6
BarmaleySPb <e.g.zakharov@gmail.com>
2021-04-19 20:17:52 +0300

    6.5.3. Сортировка выборкой. [#519568]

--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -14,4 +14,36 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSortTwo() {
+        int[] data = new int[] {3, 8, 6, 2, 7, 1, 5, 4};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortThree() {
+        int[] data = new int[] {2, 3, 5, 1, 4, 10, 7, 9, 8, 6};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortFour() {
+        int[] data = new int[] {2, 3, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortFive() {
+        int[] data = new int[] {2, 3, 5, 1, 4};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
