6c070c060a8b7692dbac7c637329e98c6e44f04d
BarmaleySPb <e.g.zakharov@gmail.com>
2021-04-19 16:50:53 +0300

    5.7. Ипотека [#519653]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,13 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = 0;
-
-        while (debt > 0 || year == 0) {
-            if (debt == 0) {
-                debt = amount * (1 + (percent / 100));
-            }
-            debt = (debt - salary) * (1 + (percent / 100));
+        while (amount > 0) {
+            amount = (amount + (amount / 100 * percent) - salary);
             year++;
         }
         return year;
