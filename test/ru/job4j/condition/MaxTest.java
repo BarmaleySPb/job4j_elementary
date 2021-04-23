4845c89a594f7004a3189ee82c9828ea844c85d0
Evgeny <e.g.zakharov@gmail.com>
2021-04-23 13:17:58 +0300

    3. Перегрузить метод max для трех чисел [#519760]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,37 +7,58 @@ public class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax5To4Then5() {
-        int left = 5;
-        int right = 4;
-        int result = Max.max(left, right);
+        int first = 5;
+        int second = 4;
+        int result = Max.max(first, second);
         int expected = 5;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax3To8Then8() {
-        int left = 3;
-        int right = 8;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 8;
+        int result = Max.max(first, second);
         int expected = 8;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax7To7Then7() {
-        int left = 7;
-        int right = 7;
-        int result = Max.max(left, right);
+        int first = 7;
+        int second = 7;
+        int result = Max.max(first, second);
         int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To7To5Then7() {
+        int first = 1;
+        int second = 7;
+        int third = 5;
+        int result = Max.max(first, second, third);
+        int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax6To2To4To9Then9() {
+        int first = 6;
+        int second = 2;
+        int third = 4;
+        int fourth = 9;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 9;
         Assert.assertEquals(result, expected);
     }
 }
